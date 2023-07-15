002c15953be0d7de1e0fefb6da69bab1fbc33770
AkiraTorres <thiago.torress@upe.br>
2023-07-14 23:28:19 -0300

    add: GET game by id

--- src/main/java/com/akira/dslist/controllers/GameController.java
+++ src/main/java/com/akira/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.akira.dslist.dto.GameDTO;
 import com.akira.dslist.dto.GameMinDTO;
 import com.akira.dslist.services.GameService;
 
@@ -17,7 +19,12 @@ public class GameController {
     private GameService gameService;
 
     @GetMapping
-    public  List<GameMinDTO> findAll() {
+    public List<GameMinDTO> findAll() {
        return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
     }
 }
