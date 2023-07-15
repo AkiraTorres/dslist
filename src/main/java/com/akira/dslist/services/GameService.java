034fbc528988f9d177120d7ab9359dcfb2814dca
AkiraTorres <thiago.torress@upe.br>
2023-07-14 23:57:28 -0300

    add: GET games by List

--- src/main/java/com/akira/dslist/services/GameService.java
+++ src/main/java/com/akira/dslist/services/GameService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.akira.dslist.entities.Game;
+import com.akira.dslist.projections.GameMinProjection;
 import com.akira.dslist.dto.GameDTO;
 import com.akira.dslist.dto.GameMinDTO;
 import com.akira.dslist.repositories.GameRepository;
@@ -26,5 +27,11 @@ public class GameService {
     public GameDTO findById(Long gameId) {
         Game result = gameRepository.findById(gameId).get();
         return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
