0cb4be4ec5d74ad0e282b8f407fe9f20549c509d
AkiraTorres <thiago.torress@upe.br>
2023-07-14 23:38:59 -0300

    GET lists

---
+++ src/main/java/com/akira/dslist/controllers/GameListController.java
@@ -0,0 +1,23 @@
+package com.akira.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.akira.dslist.dto.GameListDTO;
+import com.akira.dslist.services.GameListService;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+    @Autowired
+    private GameListService gameListService;
+
+    @GetMapping
+    public List<GameListDTO> findAll() {
+        return gameListService.findAll();
+    }
+}
